ec4a9bc2fe42c09dc09545f02aed2034d025bdd2
MattjCamp <mjcampbell74@gmail.com>
2015-06-28 11:16:10 -0400

    Added blog post about Rolling Your Own Blog

--- render-all.R
+++ render-all.R
@@ -5,4 +5,3 @@ render(template = "blog/", "blog/")
 # blog posts
 
 render(template = "blog/blog-post/", topic = "blog/why-i-am-building-my-own-blog-engine/")
-render(template = "blog/blog-post/", topic = "blog/why-you-should-avoid-seo")
