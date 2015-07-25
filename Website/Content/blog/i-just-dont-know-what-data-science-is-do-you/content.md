968284421e1cb9f1de653c082a8aec64b8bc3540
MattjCamp <mjcampbell74@gmail.com>
2015-07-25 08:40:46 -0400

    Fixed navigation bug with note template

---
+++ Website/Content/blog/i-just-dont-know-what-data-science-is-do-you/content.md
@@ -0,0 +1,4 @@
+# I Just Don't Know What Data Science Is... Do You?
+
+<img id="leftimage" src="images/ds-venn.png" width=200pt>
+
--- render-entire-website.R
+++ render-entire-website.R
@@ -1,13 +1,12 @@
 source('render.R', echo=FALSE)
 
-render(template = "blog/", "blog/")
- 
-# blog posts
+# Blog Posts
 
-# render(template = "blog/blog-post/", topic = "blog/why-i-am-building-my-own-blog-engine/")
+render(template = "blog/", "blog/")
+render(template = "blog/blog-post/", topic = "blog/data-science-certificate-program/")
+
+# Notes
 
 render(template = "notes/", topic = "notes/")
-
 render(template = "notes/note/", topic = "notes/coursera/data-science/01-02-01-Command-Line-Interface/")
 
-render(template = "blog/blog-post/", topic = "blog/data-science-certificate-program/")
