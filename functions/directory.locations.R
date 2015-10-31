2e6f71b3388cc01cbd01b54b0883b4d64101cacd
MattjCamp <mjcampbell74@gmail.com>
2015-10-31 07:57:22 -0400

    Added a R markdown file to help with testing new functions.

--- functions/directory.locations.R
+++ functions/directory.locations.R
@@ -1,19 +1,35 @@
-directory.locations <- function(parent.directory){
+# Figures out directory and file locations
+# for the topic we are working on
+
+directory.locations <- function(website.directory,
+                                parent.topic=NULL,
+                                topic=NULL){
   
   library(stringr)
 
   me <- list()
-
-  me$content.dir <- sprintf("%s/Website/Content/", getwd())
-  me$template.dir <- sprintf("%s/Website/Template/", getwd())
-  me$staged.dir <- sprintf("%s/Website/Staged/", getwd())
-  me$content.dir <- sprintf("%s%s/", content.dir, topic)
-  me$template.dir <- sprintf("%s%s/", template.dir, template)
-  me$staged.dir <- sprintf("%s%s/", staged.dir, topic)
-  me$content.dir <- str_replace_all(string = content.dir, pattern = "//", replacement = "/")
-  me$staged.dir <- str_replace_all(string = staged.dir, pattern = "//", replacement = "/")
-  me$content.file <- sprintf("%s%s", content.dir, "content.md")
-  me$staged.file <- sprintf("%s%s", staged.dir, "index.html")
+  
+  # Get top most directories
+  
+  me$content.dir <- sprintf("%s/Website/Content/",
+                            website.directory)
+  me$template.dir <- sprintf("%s/Website/Template/",
+                             website.directory)
+  me$staged.dir <- sprintf("%s/Website/Staged/",
+                           website.directory)
+  
+  # Get parent directories
+  
+#   me$content.dir <- sprintf("%s%s/", me$content.dir, topic)
+#   me$template.dir <- sprintf("%s%s/", me$template.dir, template)
+#   me$staged.dir <- sprintf("%s%s/", me$staged.dir, topic)
+#   
+#   me$content.dir <- str_replace_all(string = me$content.dir, pattern = "//", replacement = "/")
+#   me$staged.dir <- str_replace_all(string = me$staged.dir, pattern = "//", replacement = "/")
+#   
+#   
+#   me$content.file <- sprintf("%s%s", me$content.dir, "content.md")
+#   me$staged.file <- sprintf("%s%s", me$staged.dir, "index.html")
   
   class(me) <- append(class(me),"r.blog.builder.directory.locations")
   
